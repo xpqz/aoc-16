230ee7955904b0e25bba63a5864839ca75a366cd
Stefan kruger <stefan.kruger@gmail.com>
2019-04-30 17:08:10 +0100

    day09 p2

--- day09.py
+++ day09.py
@@ -2,7 +2,7 @@ import re
 
 def read_data(filename="data/input09.data"):
     with open(filename) as f:
-        return f.read().splitlines()
+        return f.read().splitlines()[0]
 
 def expand(s):
     while True:
@@ -27,9 +27,46 @@ def expand(s):
             chars -= 1
         yield data * repeat
 
+def length(s):
+
+    if s == "":
+        return 0
+
+    total = 0
+
+    pos = 0
+    while pos < len(s):
+        ch = s[pos]
+        try:
+            while ch != "(":
+                total += 1
+                pos += 1
+                ch = s[pos]
+
+            pos += 1
+        except IndexError:
+            break
+
+        spec = ""
+        while ch != ")":
+            spec += ch
+            ch = s[pos]
+            pos += 1
+
+        spec += s[pos]
+
+        chars, repeat = (int(n) for n in re.findall(r"-?\d+", spec))
+
+        total += length(s[pos:pos+chars]) * repeat
+
+        pos += chars
+
+    return total
+
 if __name__ == "__main__":
     data = read_data()
     total = 0
-    for i in expand(iter(data[0])):
+    for i in expand(iter(data)):
         total += len(i)
     print(total)
+    print(length(data))
