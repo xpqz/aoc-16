3cd096bf87f5affb68f3f73763cb40b1c996b206
Stefan kruger <stefan.kruger@gmail.com>
2019-05-21 15:35:49 +0200

    day19

--- day19.py
+++ day19.py
@@ -1,18 +1,55 @@
-def gen(elves):
-    new_elves = []
-    for pos in range(len(elves)):
-        if elves[pos]["presents"] == 0:
-            continue
-        target = (pos+1)%len(elves)
-        elves[pos]["presents"] += elves[target]["presents"]
-        new_elves.append(elves[pos])
-        elves[target]["presents"] = 0
+"""
+For part two we need a data structure that can be indexed like a list,
+but offer better than O(N) deletes. "blist" implements Indexable but
+is a btree under the hood.
 
-    return list(filter(lambda x: x["presents"]>0, new_elves))
+See https://pypi.org/project/blist/ http://stutzbachenterprises.com/blist/
+
+"""
+from blist import blist
+
+def elf_circle(length):
+    circle = blist()
+
+    for elf_id in range(1, length+1):
+        circle.append(elf_id)
+
+    return circle
+
+def steal_opposite(elfcount=3_014_603):
+    circle = elf_circle(elfcount)
+
+    pos = 0
+    while len(circle) > 1:
+        elfcount = len(circle)
+        pos %= elfcount
+        half = elfcount // 2
+        opposite = (pos + half) % elfcount
+        del circle[opposite]
+
+        if opposite > pos:
+            pos += 1
+            if pos >= len(circle):
+                pos = 0
+
+    return circle[0]
+
+
+def steal_next(elfcount=3_014_603):
+    circle = elf_circle(elfcount)
+
+    pos = 0
+    while len(circle) > 1:
+        elfcount = len(circle)
+        target = (pos+1) % elfcount
+        del circle[target]
+
+        pos = target
+
+    return circle[0]
+
 
 if __name__ == "__main__":
-    elf_count = 3014603
-    elves = [{"id": elf_id, "presents": 1} for elf_id in range(1, elf_count+1)]
-    while len(elves) > 1:
-        elves = gen(elves)
-    print(elves)
+    elf_count = 3_014_603
+    print(steal_next(elf_count))
+    print(steal_opposite(elf_count))
